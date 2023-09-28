1ea5edeba74b217674c8ed1971f778e6e2e3bb3e
elian <113941559+Zereft8@users.noreply.github.com>
2023-09-28 10:54:24 -0400

    cambio en la base

--- NorthWind/NorthWind.Domain/Core/Info.cs
+++ NorthWind/NorthWind.Domain/Core/Info.cs
@@ -5,11 +5,17 @@ namespace NorthWind.Domain.Core
     public abstract class Info : BaseEntity
     {
 
+        public string CompanyName { get; set; }
+        public string? ContactName { get; set; }
+        public string? ContactTitle { get; set; }
         public string? Address { get; set; }
         public string? City { get; set; }
         public string? Region { get; set; }
         public string? PostalCode { get; set; }
         public string? Country { get; set; }
+        public string? Phone { get; set; }
+        public string? Fax { get; set; }
+
 
     }
 }
