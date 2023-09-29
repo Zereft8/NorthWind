2973384919c4f9356d34f19f8d62931c6324bc97
Deyf3r <thekaratecat1524@gmail.com>
2023-09-28 20:47:09 -0400

    Creacion del Repositorio y la interface

---
+++ NorthWind/NorthWind.Infrastructure/Interfaces/ISuppliersRepository.cs
@@ -0,0 +1,13 @@
+﻿using NorthWind.Domain.Entities;
+using NorthWind.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthWind.Infrastructure.Interfaces
+{
+    public interface ISuppliersRepository : IBaseRepository<Suppliers>
+    {
+        List<Suppliers> GetSuppliersBySupplierID(int supplierID);
+    }
+}
---
+++ NorthWind/NorthWind.Infrastructure/Repositories/SuppliersRepository.cs
@@ -0,0 +1,41 @@
+﻿using NorthWind.Domain.Entities;
+using NorthWind.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthWind.Infrastructure.Repositories
+{
+    public class SuppliersRepository : ISuppliersRepository
+    {
+        public List<Suppliers> GetSuppliersBySupplierID(int supplierID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Suppliers> GetEntities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Suppliers GetEntity(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Suppliers entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Suppliers entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Suppliers entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
