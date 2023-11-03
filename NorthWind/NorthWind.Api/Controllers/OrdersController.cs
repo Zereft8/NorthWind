2fa1751173958e1ef44abd4a3d50fae5cfa310f2
Elianny1305 <140916417+Elianny1305@users.noreply.github.com>
2023-11-03 14:00:58 -0400

    Programacion de la capa API

---
+++ NorthWind/NorthWind.Api/Controllers/OrdersController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NorthWind.Domain.Entities;
+using NorthWind.Infrastructure.Interfaces;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NorthWind.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+
+        private readonly IOrdersRepository ordersRepository;
+
+        public OrdersController(IOrdersRepository ordersRepository)
+        {
+            this.ordersRepository = ordersRepository;
+        }
+
+        // GET: api/<OrdersController>
+        [HttpGet]
+        public IEnumerable<Orders> Get()
+        {
+            var orders = this.ordersRepository.GetOrders();
+
+            return orders;
+        }
+
+        // GET api/<OrdersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<OrdersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<OrdersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<OrdersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- NorthWind/NorthWind.Api/Program.cs
+++ NorthWind/NorthWind.Api/Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using NorthWind.Infrastructure.Context;
+using NorthWind.Infrastructure.Interfaces;
+using NorthWind.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//Agregar dependencia del contexto
+builder.Services.AddDbContext<NorthWindContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("NorthWindContext")));
+//dependencia de los repositorios
+
+builder.Services.AddTransient<IOrdersRepository, OrdersRepository>();
+
+//DEpendencia de los app services
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
